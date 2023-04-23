765ff4cd6eb6959963f46dd0848a75cb52d08819
Wooseung Cho <main.wooseung@gmail.com>
2023-04-24 00:36:44 +0900

    commit

--- rigging/advanced/p000.py
+++ rigging/advanced/p000.py
@@ -1,7 +1,5 @@
 """
-rigging 작업에서 필요한 다양한 입출력 데이터를 어떤식으로 저장해야할지 생각해보기.
+rigging 작업에서 필요한 다양한 입출력 데이터를 어떤식으로 저장해야할지 아래의 sample데이터를 참고해서 생각해보기.
 
-
-아래의 sample data 구조를 살펴보기
 https://github.com/mgear-dev/Data-centric_rigging_sample_data
 """
